9665d0465a2ff8ee4498aa0773912f55dcc299da
YuanC <jerrych.97@gmail.com>
2019-11-28 10:46:32 -0500

    Small tweaks

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -4,71 +4,34 @@ using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-[System.Serializable]
-public class Record
-{
-    // Serializable class representing a leaderboard entry
-
-    public string name;
-    public int time;
-
-    public Record(string name, int time)
-    {
-        this.name = name;
-        this.time = time;
-    }
-}
-
 public static class SaveLoad
 {
     // Class for saving and loading a persistent leaderboard
 
-    public static List<Record> Leaderboard = new List<Record>();
-    private static int MaxEntries = 10;
+    public static List<int> SaveData = new List<int>() {-1, -1, -1, -1};
 
-    // Comparison method for sorting the leaderboard entries
-    private static int CompareRecords(Record x, Record y)
+    // Adding and new save entry
+    public static void UpdateLevelMoveCount(int index, int moveCount)
     {
-        if (x.time > y.time)
+        if (index >= 0 && index < SaveData.Count)
         {
-            return 1;
-        }
-        else if (y.time > x.time)
-        {
-            return -1;
-        }
-        else
-        {
-            return 0;
+            SaveData[index] = Mathf.Min(SaveData[index], moveCount);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/SaveData.gd");
+            bf.Serialize(file, SaveLoad.SaveData);
+            file.Close();
         }
     }
 
-    // Adding and saving a new entry in the leaderboard
-    public static void UpdateLeaderboard(string name, int time)
+    // Loading the SaveData
+    public static void LoadSave()
     {
-        Leaderboard.Add(new Record(name, time));
-
-        Leaderboard.Sort(CompareRecords);
-        
-        if (Leaderboard.Count > MaxEntries)
-        {
-            Leaderboard.RemoveAt(MaxEntries);
-        }
-
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/leaderboard.gd");
-        bf.Serialize(file, SaveLoad.Leaderboard);
-        file.Close();
-    }
-
-    // Loading the leaderboard
-    public static void LoadLeaderboard()
-    {
-        if (File.Exists(Application.persistentDataPath + "/leaderboard.gd"))
+        if (File.Exists(Application.persistentDataPath + "/SaveData.gd"))
         {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/leaderboard.gd", FileMode.Open);
-            SaveLoad.Leaderboard = (List<Record>)bf.Deserialize(file);
+            FileStream file = File.Open(Application.persistentDataPath + "/SaveData.gd", FileMode.Open);
+            SaveLoad.SaveData = (List<int>)bf.Deserialize(file);
             file.Close();
         }
     }
