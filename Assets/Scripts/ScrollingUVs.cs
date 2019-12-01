fce21940b58ab760d65c829a3ebbc573d23c3cd4
YuanC <jerrych.97@gmail.com>
2019-12-01 02:35:32 -0500

    optimizely water

---
+++ Assets/Scripts/ScrollingUVs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingUVs : MonoBehaviour
+{
+    private Renderer rend;
+    private Vector2 uvOffset;
+
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        uvOffset = Vector2.zero;
+    }
+
+    void Update()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (rend.enabled)
+        {
+            rend.material.SetTextureOffset("_MainTex", uvOffset);
+        }
+    }
+}
